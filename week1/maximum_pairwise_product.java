538ebb798e54cc1c341a3a406b4c25b508ad0892
dpenkdcha <dpenkdcha@gmail.com>
2024-05-21 00:35:39 +0530

    maximum_pairwise_product

--- week1/maximum_pairwise_product.java
+++ week1/maximum_pairwise_product.java
@@ -6,19 +6,19 @@ public class maximum_pairwise_product {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
     int[] numbs = new int[n];
-    int high = 0;
-    int sHigh = 0;
+    int high = -1;
+    int sHigh = -1;
     int highPosition = -1;
     for (int i = 0; i < n; i++) {
       numbs[i] = scanner.nextInt();
-      if (i > 1 && numbs[i] > numbs[i - 1] && numbs[i] > high) {
+      if (numbs[i] > high) {
         high = numbs[i];
         highPosition = i;
       }
     }
 
 //    System.out.println(" " + high + " " + highPosition);
-    for (int x = 0; x < n - 1; x++) {
+    for (int x = 0; x < n; x++) {
       if (numbs[x] <= high && highPosition != x && numbs[x] > sHigh) {
         sHigh = numbs[x];
       }

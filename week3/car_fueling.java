5c62e0401e484a79d6b76cea94867c62bfe88e55
dpenkdcha <dpenkdcha@gmail.com>
2024-06-26 01:16:19 +0530

    week3

---
+++ week3/car_fueling.java
@@ -0,0 +1,36 @@
+package week3;
+
+import java.util.Scanner;
+
+// 3-3: Car Fueling
+public class car_fueling {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int d = scanner.nextInt();
+    int m = scanner.nextInt();
+    int n = scanner.nextInt();
+    int[] stop = new int[n];
+    int refill = 0;
+
+    for (int i = 0; i < n; i++) {
+      stop[i] = scanner.nextInt();
+    }
+
+    if (d > m) {
+      refill = d / m;
+      refill = refill-1;
+      if (d % m > 0) {
+        refill = refill + 1;
+      }
+
+      for (int i = 1; i < n; i++) {
+        if(stop[i] - stop[i-1] > m) {
+          refill = -1;
+        }
+      }
+    }
+
+    System.out.println(refill);
+
+  }
+}
---
+++ week3/fractional_knapsack.java
@@ -0,0 +1,56 @@
+package week3;
+
+import java.util.Scanner;
+
+// 3-2: Maximum Value of the Loot (Fractional Knapsack)
+public class fractional_knapsack {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int W = scanner.nextInt();
+		int[] value = new int[n];
+		int[] weight = new int[n];
+		float[] perUnitValue = new float[n];
+		int[] indx = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			int tValue = scanner.nextInt();
+			int tWeight = scanner.nextInt();
+			value[i] = tValue;
+			weight[i] = tWeight;
+			perUnitValue[i] = (float) tValue / (float) tWeight;
+			indx[i] = i;
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (perUnitValue[i] > perUnitValue[j]) {
+					float temp = perUnitValue[j];
+					perUnitValue[j] = perUnitValue[i];
+					perUnitValue[i] = temp;
+
+					int temp1 = indx[j];
+					indx[j] = indx[i];
+					indx[i] = temp1;
+				}
+			}
+		}
+
+		int count = n;
+		float totalValue = 0;
+		while (W > 0) {
+			if (W >= weight[indx[count - 1]]) {
+				totalValue = totalValue + (perUnitValue[count - 1] * weight[indx[count - 1]]);
+				W = W - weight[indx[count - 1]];
+			} else {
+				totalValue = totalValue + perUnitValue[count - 1] * W;
+				W = 0;
+			}
+			count--;
+		}
+
+		System.out.println(String.format("%.4f", totalValue));
+	}
+
+}
